692af05b79d9abe673e1fc4ca3883f76c49ecadb
kucskae <neonbolt121@hotmail.com>
2016-04-25 10:08:03 -0400

    Made General gameplay faster and made it closer to what it originally was, you should enjoy it a lot more than the 5 minute drag fest is was before. I couldn't beat my old high score.

--- app/src/main/java/cs370/team2/GameActivity.java
+++ app/src/main/java/cs370/team2/GameActivity.java
@@ -33,7 +33,7 @@ import java.util.Random;
     //private final MyGLRenderer mRenderer;
     //Random values
     Random rand = new Random();
-    double timeVal = 2000;
+    double timeVal = 1500;
     boolean end = false;
     int oldLevel=0;
     boolean ended=false;
@@ -481,16 +481,20 @@ import java.util.Random;
     void increaseSpeed(){
 
         if(level == 1) {
-            timeVal -= 525;
+            //timeVal -= 525;
+            timeVal *= (3.0/4.0);
         }
         else if (level == 2 || level == 3) {
-            timeVal -= 275;
+            //timeVal -= 275;
+            timeVal *= (5.0/6.0);
         }
         else if (level == 4 || level == 5) {
-            timeVal -=150;
+            //timeVal -=150;
+            timeVal *= (7.0/8.0);
         }
         else {
-            timeVal -= 100;
+            //timeVal -= 100;
+            timeVal *= (9.0/10.0);
         }
 
     }
